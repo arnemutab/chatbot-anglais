e92b611f455a703e8e43cbe240ca56f4b253d5b4
arnemutab <arnemutab@users.noreply.github.com>
2025-04-23 18:45:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,24 @@ st.set_page_config(page_title="Chatbot Anglais", page_icon="🇬🇧")
 st.title(" Chatbot pour apprendre l’anglais ")
 
 # Entrée utilisateur
-question = st.text_input("Pose ta question sur la grammaire, le vocabulaire ou les expressions anglaises :")
-# Chargement de la clé OpenAI (via secrets)
-openai_api_key = st.secrets["OPENAI_API_KEY"]
+def chatbot():
+    st.write ("Bot: Bonjour,Je suis ton assisant virtuel,tape 'quit' pour quitter ")
+    while True :
+        user=st.text_input("vous:").lower()
 
-# LangChain - LLM
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7, openai_api_key=openai_api_key)
+        if user=="quit":
+            st.write("Bot: À bientôt ! 👋")
+            break
+        elif "bonjour" in user:
+            st.write("Bot: Bonjour à toi aussi")
+        elif "Comment ça va" in user:
+            st.write("Bot: Je vais bien, merci. Et toi ?")
+        elif "ton nom" in user:
+            st.write("Bot:je m'appelle botos")
+        else :
+            st.write("Bot: Désolé, je n'ai pas compris. 😕")
 
-# Prompt LangChain
-prompt = PromptTemplate(
-    input_variables=["question"],
-    template="Tu es un professeur d’anglais patient et sympathique. Réponds à cette question : {question}"
-)
-
-# Chaîne LangChain
-chain = LLMChain(llm=llm, prompt=prompt)
-
-# Exécution
-if question:
-    with st.spinner("Le professeur réfléchit... 🧠"):
-        response = chain.run(question)
-        st.write("✏️ Réponse :", response)
+chatbot()
 
 
 
